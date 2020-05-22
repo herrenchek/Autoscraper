009d2f05876452198145b369a24687ddc444112b
Giselle Malenchek <gisellem@NYCPV14H.fios-router.home>
2020-05-21 22:02:46 -0400

    Working on titles

--- soup.py
+++ soup.py
@@ -1,12 +1,18 @@
 import csv, requests
 from bs4 import BeautifulSoup
 
-resp = requests.get('https://www.reuters.com/subjects/autos', headers={'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'})
+# News source
+url = 'https://www.reuters.com/subjects/autos'
+resp = requests.get(url, headers={'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'})
 
 resp.raise_for_status()
 
 soup = BeautifulSoup(resp.text, 'html.parser')
 
+# Reuters news section
+# news = soup.find('div', {'class': 'column1'}).findAll('section')
+
+# Article summary
 p = soup.select('#content > section:nth-child(4) > div > div.column1.col.col-10 > section:nth-child(2) > section > div > article:nth-child(15) > div.story-content > p')
 summary = p[0].text.strip()
 
@@ -14,7 +20,7 @@ file = open('autonews.csv', 'w')
 
 writer = csv.writer(file)
 # Write header rows
-writer.writerow(['Title', 'Summary'])
-writer.writerow([summary])
+writer.writerow(['Source', 'Title', 'Summary'])
+writer.writerow([url, summary])
 
 file.close()
