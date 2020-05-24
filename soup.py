e144b701f1af78909e30752700ad27d05562ef08
Giselle Malenchek <gisellem@NYCPV14H.fios-router.home>
2020-05-23 20:12:42 -0400

    Headline and summary found

--- soup.py
+++ soup.py
@@ -10,17 +10,23 @@ resp.raise_for_status()
 soup = BeautifulSoup(resp.text, 'html.parser')
 
 # Reuters news section
-# news = soup.find('div', {'class': 'column1'}).findAll('section')
+# Returns list containing each article
+# articles = soup.find('div', {'class': 'column1'}).findAll('article', {'class': 'story'})
 
-# Article summary
-p = soup.select('#content > section:nth-child(4) > div > div.column1.col.col-10 > section:nth-child(2) > section > div > article:nth-child(15) > div.story-content > p')
-summary = p[0].text.strip()
+# Article titles
+titles = soup.find('div', {'class': 'column1'}).findAll(True, {'class': 'story-title'})
+title = titles[0].text.strip()
+
+# Article summaries
+summaries = soup.find('div', {'class': 'column1'}).findAll('p')
+summary = summaries[0].text.strip()
 
 file = open('autonews.csv', 'w')
 
 writer = csv.writer(file)
 # Write header rows
 writer.writerow(['Source', 'Title', 'Summary'])
-writer.writerow([url, summary])
+
+writer.writerow([url, title, summary])
 
 file.close()
