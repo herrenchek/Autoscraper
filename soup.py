4d0328280676ea798aeedd789e898bf231d8f2ce
Giselle Malenchek <gisellem@NYCPV14H.fios-router.home>
2020-05-26 12:09:00 -0400

    FINALLY

--- soup.py
+++ soup.py
@@ -13,8 +13,22 @@ soup = BeautifulSoup(resp.text, 'html.parser')
 # Returns list containing each Reuters article
 articles = soup.find('div', {'class': 'column1'}).findAll('article', {'class': 'story'})
 
-# Article URL
-source = url + articles[1].div.a['href']
+file = open('autonews.csv', 'w')
+
+writer = csv.writer(file)
+# Write header rows
+writer.writerow(['Source', 'Title', 'Summary'])
+
+# Iterating through each article in the list
+for article in articles:
+    # Article URL
+    source = url + article.div.a['href']
+    # Article title
+    title = article.h3
+    # Article summary
+    summary = article.p
+
+    writer.writerow([source, title, summary])
 
 # Article titles
 titles = soup.find('div', {'class': 'column1'}).findAll(True, {'class': 'story-title'})
@@ -22,18 +36,11 @@ titles = soup.find('div', {'class': 'column1'}).findAll(True, {'class': 'story-t
 
 # Article summaries
 summaries = soup.find('div', {'class': 'column1'}).findAll('p')
-summary = summaries[0].text.strip()
+# summary = summaries[0].text.strip()
 
-file = open('autonews.csv', 'w')
-
-writer = csv.writer(file)
-# Write header rows
-writer.writerow(['Source', 'Title', 'Summary'])
-
-for title in titles:
-    title = title.text.strip()
-
-    writer.writerow([title])
+# for title in titles:
+    # title = title.text.strip()
+    # writer.writerow([title])
     # writer.writerow([source, title, summary])
 
 file.close()
