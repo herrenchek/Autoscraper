d91d732fe7647c445709c9593ddd7b1208cf8e6d
Giselle Malenchek <gisellem@NYCPV14H.fios-router.home>
2020-05-27 21:24:06 -0400

    Edmunds added

--- soup.py
+++ soup.py
@@ -12,6 +12,7 @@ soup = BeautifulSoup(resp.text, 'html.parser')
 
 # Returns list containing each Reuters article
 divs = soup.find('div', {'class': 'column1'}).findAll('div', {'class': 'story-content'})
+
 # Article titles
 # titles = soup.find('div', {'class': 'column1'}).findAll(True, {'class': 'story-title'})
 # Article summaries
@@ -23,15 +24,32 @@ writer = csv.writer(file)
 # Write header rows
 writer.writerow(['Source', 'Title', 'Summary'])
 
-# Iterating through each article in the list
+# Iterating through each Reuters div in the list
 for div in divs:
     # Article URL
     source = url + div.a['href']
     # Article title
     title = div.a.text.strip()
     # Article summary
-    summary = div.text.strip()
+    summary = div.p
 
     writer.writerow([source, title, summary])
+
+# Edmunds RSS feed
+ed_r = requests.get('https://www.edmunds.com/feeds/rss/car-news.xml', headers={'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 8_3 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) FxiOS/1.0 Mobile/12F69 Safari/600.1.4'})
+
+ed_r.raise_for_status()
+
+ed_soup = BeautifulSoup(ed_r.content, 'html5lib')
+
+# Returns list containing each item in the Edmunds XML feed
+items = ed_soup.findAll('item')
+
+# Iterating through each Edmunds item
+for item in items:
+    source = item.guid.text
+    title = item.title.text
+
+    writer.writerow([source, title])
 
 file.close()
